48b13ec15e038d27823e9e2637259578ac73e8cd
samir419 <takashisamir@gmail.com>
2025-12-05 15:53:54 +0000

    added some short stories

--- rpg2/tab_handler.js
+++
@@ -1,8 +0,0 @@
- function openTab(evt, tabName) {
-      // Hide all tab content
-      document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-      // Show selected tab
-      document.getElementById(tabName).classList.add('active');
-  }
-
- tabbing = true
